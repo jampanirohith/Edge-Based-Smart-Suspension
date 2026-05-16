ef0a4c98e73956c5ffefbeb5848a2baa6cd93296
jampanirohith <jampanirohithchhowdary@gmail.com>
2026-05-16 14:36:23 +0530

    160520262

--- download_dataset.py
+++ download_dataset.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="xlS3pb3QgzjFDpIbMZlJ")
+rf = Roboflow(api_key="")
 project = rf.workspace("pothole-detector-dmdod").project("pothole-clzln")
 version = project.version(1)
 dataset = version.download("yolov8")
